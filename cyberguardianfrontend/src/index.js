61902dd72bc6c9e572cedab0ef1950f02b48fb8b
creely001 <creely001@gannon.edu>
2022-11-27 18:10:38 -0500

    added project structure files

--- cyberguardianfrontend/src/index.js
+++ cyberguardianfrontend/src/index.js
@@ -11,26 +11,6 @@ root.render(
   </React.StrictMode>
 );
 
-const express = require('express'),
-http = require('http');
- 
-const hostname = 'localhost';
-const port = 8080;
-const app = express();
- 
-app.use((req, res) => {
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is a test server</h1></body></html>');
- 
-});
-const sample_server = http.createServer(app);
- 
-sample_server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
